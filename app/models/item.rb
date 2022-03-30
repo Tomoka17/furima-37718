ff9a4d7ac546520e1556cedfe2e5e4d119c286eb
Tomoka17 <tomotomato.617@gmail.com>
2022-03-31 08:56:46 +0900

    バリデーション、アソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :user, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :item_status_id, presence: true
+  validates :freight_id, presence: true
+  validates :ship_from_id, presence: true
+  validates :day_to_ship_id, presence: true
+
+  belongs_to :user
+  # has_one :order
 end
