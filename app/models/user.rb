fe94a8ebc59395c731df0d84e2b7048cdf3cd5e1
Tomoka17 <tomotomato.617@gmail.com>
2022-03-30 16:21:36 +0900

    Rubocup実施

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,16 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
-  validates :password, presence: true,format: { with: VALID_PASSWORD_REGEX }
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
 
-  #has_many :items
-  #has_many :orders
+  # has_many :items
+  # has_many :orders
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,10 +6,10 @@ FactoryBot.define do
     first_name_kana { japanese_user.first.katakana }
     last_name       { japanese_user.last.kanji }
     last_name_kana  { japanese_user.last.katakana }
-    email           { Faker::Internet.free_email}
+    email           { Faker::Internet.free_email }
     password        { Faker::Internet.password }
-    password_confirmation {password}
-    nickname        { Faker::Name.initials(number: 2)}
-    birthday        {Faker::Date.birthday(min_age: 5, max_age: 92)}
+    password_confirmation { password }
+    nickname        { Faker::Name.initials(number: 2) }
+    birthday        { Faker::Date.birthday(min_age: 5, max_age: 92) }
   end
 end
