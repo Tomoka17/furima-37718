801dfe97001b287d6a07df90a99a9ec37e63964e
Tomoka17 <tomotomato.617@gmail.com>
2022-03-31 14:37:58 +0900

    priceバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,9 @@ class Item < ApplicationRecord
   belongs_to :category, :day_to_ship, :freight, :item_status, :ship_from
 
   validates :name, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/i },numericality: {
+    only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999
+  }
   validates :user, presence: true
   validates :description, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
