9233fdc628dbe06957891d98b3a01a1d08de528d
Tomoka17 <tomotomato.617@gmail.com>
2022-03-31 10:42:51 +0900

    activhash導入、アソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,13 +3,17 @@ class Item < ApplicationRecord
   validates :price, presence: true
   validates :user, presence: true
   validates :description, presence: true
-  validates :category_id, presence: true
-  validates :item_status_id, presence: true
-  validates :freight_id, presence: true
-  validates :ship_from_id, presence: true
-  validates :day_to_ship_id, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :item_status_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :freight_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :ship_from_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :day_to_ship_id, numericality: { other_than: 1, message: "can't be blank" } 
 
   belongs_to :user
   has_one_attached :image
   # has_one :order
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :day_to_ship, :freight, :item_status, :ship_from
+
 end
