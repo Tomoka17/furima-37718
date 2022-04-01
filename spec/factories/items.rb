d581aaf6af4ee6165e79e99b746eb6f915c1f5bb
Tomoka17 <tomotomato.617@gmail.com>
2022-04-01 14:17:16 +0900

    ファクトリーボット修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,16 @@
 FactoryBot.define do
   factory :item do
-    name        {Faker::Commerce.product_name}
-    price       {Faker::Commerce.price(range:300..9999999)}
-    description {Faker::Lorem.sentence}
+    name           {Faker::Commerce.product_name}
+    price          {300}
+    description    {Faker::Lorem.sentence}
     association :user
-    category    {2}
-    item_status {2}
-    freight     {2}
-    ship_from   {2}
-    day_to_ship {2}
+    category_id    { Faker::Number.between(from: 2, to: 11) }
+    item_status_id { Faker::Number.between(from: 2, to: 7) }
+    freight_id     { Faker::Number.between(from: 2, to: 3) }
+    ship_from_id   { Faker::Number.between(from: 2, to: 48) }
+    day_to_ship_id { Faker::Number.between(from: 2, to: 4) }
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test.png'),filename: 'test.png')
   end
+  end 
 end
